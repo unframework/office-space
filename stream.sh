afd51b4681435ca7e94413dd2ea933ad77dd7b71
Nick Matantsev <nick.matantsev@gmail.com>
2017-05-22 17:07:09 -0400

    More tweaks to streaming settings

--- stream.sh
+++ stream.sh
@@ -1,18 +1,22 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-VBR="2500k"
+# Chrome should be run as:
+# /c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe --kiosk --user-data-dir=chrome-user-data --no-first-run http://localhost:9966
+
 FPS="30"
 QUAL="fast"
 YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
 WINDOW_TITLE='budo - Google Chrome'
-FFMPEG='/c/Users/Nikita/Desktop/ffmpeg-3.3.1-win64-static/bin/ffmpeg'
+FFMPEG="$HOME/Desktop/ffmpeg-3.3.1-win64-static/bin/ffmpeg"
 
 "$FFMPEG" \
-    -re \
     -f gdigrab -framerate "$FPS" \
     -offset_x 0 -offset_y 0 -video_size 1280x720 \
     -i title="$WINDOW_TITLE" \
-    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
-    -acodec libmp3lame -ar 44100 -threads 6 -crf 20 -b:a 712000 -bufsize 512k \
+    -vcodec libx264 -pix_fmt yuv420p -preset fast -crf 20 -r $FPS -b:v 2500k \
+    -acodec libmp3lame -ar 44100 -b:a 712000 \
     -f flv \
     "$YOUTUBE_URL/$KEY"
+
+    # -bufsize 512k \
+    # -threads 6 \
