4e09dac81e26ffca579b73ad3da3415cc6f80d20
NM <mmatants@bitcurve.com>
2017-05-22 15:03:57 -0400

    Tweak streaming settings

--- stream.sh
+++ stream.sh
@@ -4,11 +4,15 @@ VBR="2500k"
 FPS="30"
 QUAL="medium"
 YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
+WINDOW_TITLE='budo - Google Chrome'
+FFMPEG='/c/Users/Nikita/Desktop/ffmpeg-3.3.1-win64-static/bin/ffmpeg'
 
-ffmpeg \
+"$FFMPEG" \
     -re \
     -f gdigrab -framerate "$FPS" \
-    -i title='budo - Google Chrome' -offset_x 0 -offset_y 0 -video_size 1280x720
+    -offset_x 0 -offset_y 0 -video_size 1280x720 \
+    -i title="$WINDOW_TITLE" \
     -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
     -acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 712000 -bufsize 512k \
-    -f flv "$YOUTUBE_URL/$KEY"
+    -f flv \
+    "$YOUTUBE_URL/$KEY"
